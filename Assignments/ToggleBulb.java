3277099ee5feb041d3245a772c2d21ea68c0447e
Adarsh Rangare <akrangare@gmail.com>
2023-08-21 18:56:13 +0530

    Create ToggleBulb.java

---
+++ Assignments/ToggleBulb.java
@@ -0,0 +1,15 @@
+// https://my.newtonschool.co/playground/code/wg7ug924jo8m
+class ToggleBulb {
+public int bulbSwitch(int n) {
+    // enter your code here
+
+    int on =0;
+    for(int i=1;i*i<=n;i++)]
+    
+        on++;
+
+
+    }
+    return on;
+    }
+}
