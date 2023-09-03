073c894b561535f16b309dc1e6095580a50c49c1
Adarsh Rangare <akrangare@gmail.com>
2023-09-03 22:48:13 +0530

    Create  PairHate.java

---
+++ Contest/DSA-2/PairHate.java
@@ -0,0 +1,32 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop()); // Reverse the order to maintain the original order
+        }
+
+             System.out.print(result.toString());
+    
+
+    }
+}
